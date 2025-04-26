efb9dbb97bcc490355049aff01735f55103f3e6d
Huiyeongkim <huiyeong9619@naver.com>
2025-04-26 18:14:21 +0900

    fix: redis 삭제

--- src/main/java/travel/travel/plan/service/LikeService.java
+++ src/main/java/travel/travel/plan/service/LikeService.java
@@ -4,7 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
+// import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import travel.travel.member.domain.Member;
@@ -19,7 +19,7 @@ import travel.travel.plan.repository.PlanRepository;
 @RequiredArgsConstructor
 @Slf4j
 public class LikeService {
-    private final RedisTemplate<String, Object> redisTemplate;
+   //  private final RedisTemplate<String, Object> redisTemplate;
 
     private final PlanRepository planRepository;
     private final MemberRepository memberRepository;
@@ -28,42 +28,43 @@ public class LikeService {
     private static final String LIKE_USERS_KEY = "likes:post:";
 
     public boolean toggleLike(Long postId) {
-        Plan existingPlan = planRepository.findById(postId)
-                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 계획입니다."));
-
-        String memberId = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Member member = memberRepository.findById(Long.valueOf(memberId))
-                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
-
-        String likeUserKey = LIKE_USERS_KEY + postId;
-        String likeCountKey = LIKE_COUNT_KEY + postId;
-
-        Boolean isLiked = redisTemplate.opsForSet().isMember(likeUserKey, member.getId());
-
-        if (Boolean.TRUE.equals(isLiked)) {
-            // 좋아요 취소
-            redisTemplate.opsForSet().remove(likeUserKey, member.getId());
-            redisTemplate.opsForValue().decrement(likeCountKey);
-            return false;
-        }
+//        Plan existingPlan = planRepository.findById(postId)
+//                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 계획입니다."));
+//
+//        String memberId = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        Member member = memberRepository.findById(Long.valueOf(memberId))
+//                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+//
+//        String likeUserKey = LIKE_USERS_KEY + postId;
+//        String likeCountKey = LIKE_COUNT_KEY + postId;
+//
+//        Boolean isLiked = redisTemplate.opsForSet().isMember(likeUserKey, member.getId());
+//
+//        if (Boolean.TRUE.equals(isLiked)) {
+//            // 좋아요 취소
+//            redisTemplate.opsForSet().remove(likeUserKey, member.getId());
+//            redisTemplate.opsForValue().decrement(likeCountKey);
+//            return false;
+//        }
 
         // 좋아요 추가
-        redisTemplate.opsForSet().add(likeUserKey, member.getId());
-        redisTemplate.opsForValue().increment(likeCountKey);
+//        redisTemplate.opsForSet().add(likeUserKey, member.getId());
+//        redisTemplate.opsForValue().increment(likeCountKey);
         return true;
     }
 
     // 좋아요 개수 조회
     public Long getLikeCount(Long postId) {
-        Plan existingPlan = planRepository.findById(postId)
-                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 계획입니다."));
-
-        String likeCountKey = LIKE_COUNT_KEY + postId;
-        Object count = redisTemplate.opsForValue().get(likeCountKey);
-        if (count == null) {
-            return 0L;
-        }
-        return Long.parseLong(count.toString());
+//        Plan existingPlan = planRepository.findById(postId)
+//                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 계획입니다."));
+//
+//        String likeCountKey = LIKE_COUNT_KEY + postId;
+//        Object count = redisTemplate.opsForValue().get(likeCountKey);
+//        if (count == null) {
+//            return 0L;
+//        }
+//        return Long.parseLong(count.toString());
+        return postId;
     }
 
 }
