a243fc9c9b20f32d365504c871c7494d0032246e
Huiyeongkim <huiyeong9619@naver.com>
2025-05-07 12:36:19 +0900

    feat: cors 설정 배포버전 추가

--- src/main/java/travel/travel/common/config/CorsConfig.java
+++ src/main/java/travel/travel/common/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package travel.travel.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,10 +13,13 @@ import java.util.List;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
